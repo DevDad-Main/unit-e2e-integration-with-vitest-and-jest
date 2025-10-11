3b266c59818f3d0be694d5c91d113eaa477cd8da
Oliver Metz <softwaredevdad@gmail.com>
2025-10-11 17:02:26 +0200

    add some extra unit tests which allows to discover a bug/fault in our code, so we ensure in our add function we convert the number wiht + so we don't get NaN

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -12,3 +12,21 @@ it("should summarize all number values in array", () => {
   const expectedResult = numbers.reduce((acc, cur) => acc + cur, 0);
   expect(result).toBe(expectedResult);
 });
+
+it("should yield NaN if at least one invalid number is provided", () => {
+  // Arrange
+  const inputs = ["Invalid", 1];
+
+  const result = add(inputs);
+
+  expect(result).toBeNaN(result);
+});
+
+it("should yield a correct sum if an array of numeric string values is provided", () => {
+  const numbers = ["1", "2", "3"];
+
+  const result = add(numbers);
+
+  const expectedResult = numbers.reduce((acc, cur) => +acc + +cur, 0);
+  expect(result).toBe(expectedResult);
+});
