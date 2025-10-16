1014c705f424e2e8c9673353f7df79dc6436a2b5
Oliver Metz <softwaredevdad@gmail.com>
2025-10-16 23:07:51 +0200

    add a test suite for ValidationError class and a couple unit tests

--- mocking-and-diving-deeper/util/errors.test.js
+++ mocking-and-diving-deeper/util/errors.test.js
@@ -2,16 +2,26 @@ import { it, expect, describe } from "vitest";
 import { HttpError, ValidationError } from "./errors";
 
 describe("ValidationError", () => {
-  it("should create an instance with message and details", () => {
+  it("should create an instance with a message", () => {
     const message = "Invalid input";
-    const details = { field: "username", issue: "required" };
-    const error = new ValidationError(message, details);
+    const error = new ValidationError(message);
 
-    console.log(error.name);
-
+    expect(error.message);
     expect(error).toBeInstanceOf(ValidationError);
     expect(error.message).toBe(message);
-    expect(error.details).toBe(details);
-    expect(error.name).toBe("ValidationError");
+  });
+
+  it("should return an error if message is not of type string", () => {
+    const message = "an error message";
+    const error = new ValidationError(message);
+
+    expect(error.message).toBeTypeOf("string");
+  });
+
+  it("should return an error if the message is an empty string", () => {
+    const message = "";
+    const error = new ValidationError(message);
+
+    expect(error.message).toBe("");
   });
 });
