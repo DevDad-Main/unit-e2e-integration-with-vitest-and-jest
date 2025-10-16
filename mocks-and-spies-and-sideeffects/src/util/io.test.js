5b6883cfd1eadf9eb2766246a87e2b9ba69b8f07
Oliver Metz <softwaredevdad@gmail.com>
2025-10-16 13:08:13 +0200

    add a new directory to contain all of our mocks so we dont pollute test scripts, add factory callback so we can determine our own logic for certain submodules of a package

--- mocks-and-spies-and-sideeffects/src/util/io.test.js
+++ mocks-and-spies-and-sideeffects/src/util/io.test.js
@@ -1,10 +1,20 @@
 import { it, expect, vi } from "vitest";
 import writeData from "./io";
 import { promises as fs } from "fs";
+import { join } from "path";
 
 //#region NOTE: Here, we are mocking the fs module. This means that we can replace the functionality of all the modules functions with empty spies. In this case, we are replacing the writeFile function with a mock function that simply returns undefined.
 //#endregion
 vi.mock("fs");
+vi.mock("path", () => {
+  return {
+    default: {
+      join: (...args) => {
+        return args[args.length - 1];
+      },
+    },
+  };
+});
 
 //#region NOTE: Here, we have the problem that I don't wanna execute the original writeFile function or method. Instead, we only wanna find out if it was called, but it shouldn't do its job of writing to the file system. It should do that during production, but not when we run our tests. This also sounds like the perfect use case and scenario for a spy, right?.
 
