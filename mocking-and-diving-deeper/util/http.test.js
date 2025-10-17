156445fc4dbd345f98ecfbd2f181a3c57d10641c
Oliver Metz <softwaredevdad@gmail.com>
2025-10-17 21:10:19 +0200

    add another unit test to our test suite for the fetch function, where we check to see if the data has not been stringified then we will caatch that error in our block, added lots of notes due to the way the expect and rejects work.

--- mocking-and-diving-deeper/util/http.test.js
+++ mocking-and-diving-deeper/util/http.test.js
@@ -6,6 +6,10 @@ const testResponseData = { testKey: "testValue" };
 //NOTE: Designing our own fetch mock method to avoid the need for a real fetch request and also because we cant mock the fetch package for exmaple like we have done in the past due to fetch being a globally available function
 const testFetch = vi.fn((url, options) => {
   return new Promise((resolve, reject) => {
+    if (typeof options.body !== "string") {
+      return reject("Not a String.");
+    }
+
     const testResponse = {
       ok: true,
       json() {
@@ -26,5 +30,25 @@ describe("sendDataRequest()", () => {
     const testData = { key: "value" };
 
     return expect(sendDataRequest(testData)).resolves.toEqual(testResponseData);
+  });
+
+  it("should convert the provided data to json before sending the requst", async () => {
+    const testData = { key: "value" };
+
+    //#region NOTE: The issue is with this is that if the data is parsed as a string and we have stringified data then we would assume the test would pass, but the issue is then is that the below expectation will then resolve.
+    //NOTE: This dosen't check to see if we resolve, this only checks for this .not.rejects with the toBe("Not a String.") above, Expects to not reject to this value, not to not reject overall
+    //#endregion
+    // return expect(sendDataRequest(testData)).not.rejects.toBe("Not a String.");
+
+    //NOTE: Better way to handle this is to use async await and wrap it in a trycatch block. This now will allow our test to pass even if we do resolve. But if our data wasn't stringified correctly then we would fail this test as we catch that error in our catch block and errorMessage will be Not a String.
+    let errorMessage;
+
+    try {
+      await sendDataRequest(testData).not.rejects.toBe("Not a String.");
+    } catch (error) {
+      errorMessage = error;
+    }
+
+    expect(errorMessage).not.toBe("Not a String.");
   });
 });
